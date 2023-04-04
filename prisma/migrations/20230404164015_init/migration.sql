200214774f80834ba5eab387f32356e6ea7e6099
Ha Phi Hung <haphihung55@gmail.com>
2023-04-04 23:43:03 +0700

    Implement database schema

---
+++ prisma/migrations/20230404164015_init/migration.sql
@@ -0,0 +1,283 @@
+/*
+  Warnings:
+
+  - Added the required column `birthDate` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `type` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "UserType" AS ENUM ('Regular', 'Critic');
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "birthDate" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "type" "UserType" NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Genre" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Genre_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Company" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Company_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CrewMember" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "CrewMember_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ActingCredit" (
+    "crewId" INTEGER NOT NULL,
+    "movieId" INTEGER NOT NULL,
+    "characterName" TEXT NOT NULL,
+
+    CONSTRAINT "ActingCredit_pkey" PRIMARY KEY ("crewId","movieId")
+);
+
+-- CreateTable
+CREATE TABLE "Movie" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "releaseDate" TIMESTAMP(3) NOT NULL,
+    "reviewCount" INTEGER NOT NULL,
+    "userScore" DOUBLE PRECISION NOT NULL,
+    "userReviewCount" INTEGER NOT NULL,
+    "criticScore" DOUBLE PRECISION NOT NULL,
+    "criticReviewCount" INTEGER NOT NULL,
+
+    CONSTRAINT "Movie_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Review" (
+    "id" SERIAL NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "movieId" INTEGER NOT NULL,
+    "postTime" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "score" INTEGER NOT NULL,
+    "content" TEXT NOT NULL,
+    "likeCount" INTEGER NOT NULL,
+    "commentCount" INTEGER NOT NULL,
+
+    CONSTRAINT "Review_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" SERIAL NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "reviewId" INTEGER NOT NULL,
+    "postTime" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "content" TEXT NOT NULL,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_GenreToMovie" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_production-company" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_distribution-company" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_director" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_writer" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_dop" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_composer" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_editor" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_MovieToUser" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_review-likes" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_GenreToMovie_AB_unique" ON "_GenreToMovie"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_GenreToMovie_B_index" ON "_GenreToMovie"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_production-company_AB_unique" ON "_production-company"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_production-company_B_index" ON "_production-company"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_distribution-company_AB_unique" ON "_distribution-company"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_distribution-company_B_index" ON "_distribution-company"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_director_AB_unique" ON "_director"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_director_B_index" ON "_director"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_writer_AB_unique" ON "_writer"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_writer_B_index" ON "_writer"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_dop_AB_unique" ON "_dop"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_dop_B_index" ON "_dop"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_composer_AB_unique" ON "_composer"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_composer_B_index" ON "_composer"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_editor_AB_unique" ON "_editor"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_editor_B_index" ON "_editor"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_MovieToUser_AB_unique" ON "_MovieToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_MovieToUser_B_index" ON "_MovieToUser"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_review-likes_AB_unique" ON "_review-likes"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_review-likes_B_index" ON "_review-likes"("B");
+
+-- AddForeignKey
+ALTER TABLE "ActingCredit" ADD CONSTRAINT "ActingCredit_crewId_fkey" FOREIGN KEY ("crewId") REFERENCES "CrewMember"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ActingCredit" ADD CONSTRAINT "ActingCredit_movieId_fkey" FOREIGN KEY ("movieId") REFERENCES "Movie"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_movieId_fkey" FOREIGN KEY ("movieId") REFERENCES "Movie"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_reviewId_fkey" FOREIGN KEY ("reviewId") REFERENCES "Review"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_GenreToMovie" ADD CONSTRAINT "_GenreToMovie_A_fkey" FOREIGN KEY ("A") REFERENCES "Genre"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_GenreToMovie" ADD CONSTRAINT "_GenreToMovie_B_fkey" FOREIGN KEY ("B") REFERENCES "Movie"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_production-company" ADD CONSTRAINT "_production-company_A_fkey" FOREIGN KEY ("A") REFERENCES "Company"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_production-company" ADD CONSTRAINT "_production-company_B_fkey" FOREIGN KEY ("B") REFERENCES "Movie"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_distribution-company" ADD CONSTRAINT "_distribution-company_A_fkey" FOREIGN KEY ("A") REFERENCES "Company"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_distribution-company" ADD CONSTRAINT "_distribution-company_B_fkey" FOREIGN KEY ("B") REFERENCES "Movie"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_director" ADD CONSTRAINT "_director_A_fkey" FOREIGN KEY ("A") REFERENCES "CrewMember"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_director" ADD CONSTRAINT "_director_B_fkey" FOREIGN KEY ("B") REFERENCES "Movie"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_writer" ADD CONSTRAINT "_writer_A_fkey" FOREIGN KEY ("A") REFERENCES "CrewMember"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_writer" ADD CONSTRAINT "_writer_B_fkey" FOREIGN KEY ("B") REFERENCES "Movie"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_dop" ADD CONSTRAINT "_dop_A_fkey" FOREIGN KEY ("A") REFERENCES "CrewMember"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_dop" ADD CONSTRAINT "_dop_B_fkey" FOREIGN KEY ("B") REFERENCES "Movie"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_composer" ADD CONSTRAINT "_composer_A_fkey" FOREIGN KEY ("A") REFERENCES "CrewMember"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_composer" ADD CONSTRAINT "_composer_B_fkey" FOREIGN KEY ("B") REFERENCES "Movie"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_editor" ADD CONSTRAINT "_editor_A_fkey" FOREIGN KEY ("A") REFERENCES "CrewMember"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_editor" ADD CONSTRAINT "_editor_B_fkey" FOREIGN KEY ("B") REFERENCES "Movie"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_MovieToUser" ADD CONSTRAINT "_MovieToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Movie"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_MovieToUser" ADD CONSTRAINT "_MovieToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_review-likes" ADD CONSTRAINT "_review-likes_A_fkey" FOREIGN KEY ("A") REFERENCES "Review"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_review-likes" ADD CONSTRAINT "_review-likes_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
