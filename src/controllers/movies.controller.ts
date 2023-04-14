395afe4b5b9b7a5ae0cca01b191dbdd1375064b7
Ha Phi Hung <haphihung55@gmail.com>
2023-04-14 22:56:27 +0700

    Implement get all movies endpoint

--- src/controllers/movies.controller.ts
+++ src/controllers/movies.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { prismaClient } from '../db';
 
 export async function getMovies(req: Request, res: Response) {
-  const searchTerm = '';
+  const searchTerm = req.query.searchTerm as string | undefined;
   const result = await prismaClient.movie.findMany({
     where: {
       title: { contains: searchTerm, mode: 'insensitive' },
--- src/routes/movies.route.ts
+++ src/routes/movies.route.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
 import { getMovies } from '../controllers/movies.controller';
-import passport from 'passport';
 
 const router = Router();
 
-router.get('/', passport.authenticate('jwt', { session: false }), getMovies);
+router.get('/', getMovies);
 
 export default router;
