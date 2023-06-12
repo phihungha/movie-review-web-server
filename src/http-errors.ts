dba09652521ee476a9e488982ee41c61754c780a
Ha Phi Hung <haphihung55@gmail.com>
2023-06-12 23:43:23 +0700

    Improve error name

--- src/http-errors.ts
+++ src/http-errors.ts
@@ -1,5 +1,6 @@
 export class HttpError extends Error {
   statusCode: number;
+  name = 'HttpError';
   constructor(statusCode: number, message: string) {
     super(message);
     this.statusCode = statusCode;
@@ -7,18 +8,21 @@ export class HttpError extends Error {
 }
 
 export class HttpNotFoundError extends HttpError {
+  name = 'HttpNotFoundError';
   constructor(message: string) {
     super(404, message);
   }
 }
 
 export class HttpForbiddenError extends HttpError {
+  name = 'HttpForbiddenError';
   constructor(message: string) {
     super(403, message);
   }
 }
 
 export class HttpBadRequest extends HttpError {
+  name = 'HttpBadRequest';
   constructor(message: string) {
     super(400, message);
   }
