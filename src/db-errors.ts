c5fb54be5e5741464ea8a2de63721f9f9cb57f58
Ha Phi Hung <haphihung55@gmail.com>
2023-07-09 21:06:51 +0700

    Add documentation for db error handlera

--- src/db-errors.ts
+++ src/db-errors.ts
@@ -2,9 +2,26 @@ import { Prisma } from '@prisma/client';
 import { NextFunction } from 'express';
 import { HttpBadRequest, HttpNotFoundError } from './http-errors';
 
+/**
+ * Xử lý exception từ database client (Prisma) để throw HttpException phù hợp.
+ * Dùng Chain of Responsibility, Template method, Builder.
+ */
+
+/**
+ * Base class cho Chain of Responsibility.
+ * Mỗi lớp ErrorHandler sẽ kiểm tra lỗi có xử lý được không,
+ * nếu được thì xử lý (bằng cách throw HttpException),
+ * không thì chuyển cho ErrorHandler tiếp theo.
+ */
 abstract class DbErrorHandler {
   constructor(public message?: string, public nextHandler?: DbErrorHandler) {}
 
+  /**
+   * Cách xử lý của mỗi ErrorHandler chỉ khác nhau về
+   * điều kiện lỗi và exception được throw lại
+   * nên dùng template method ở đây để các lớp con override
+   * chỗ check lỗi và throw lại.
+   */
   handle(error: unknown, nextMiddleware: NextFunction) {
     if (this.checkError(error)) {
       nextMiddleware(this.rethrow(error));
@@ -19,6 +36,7 @@ abstract class DbErrorHandler {
   abstract rethrow(error: unknown): Error;
 }
 
+// Xử lý lỗi không tìm thấy ID
 export class DbNotFoundErrorHandler extends DbErrorHandler {
   constructor(message?: string, nextHandler?: DbErrorHandler) {
     super(message, nextHandler);
@@ -39,6 +57,7 @@ export class DbNotFoundErrorHandler extends DbErrorHandler {
   }
 }
 
+// Xử lý lỗi vi phạm unique constraint
 export class DbUniqueErrorHandler extends DbErrorHandler {
   constructor(message?: string, nextHandler?: DbErrorHandler) {
     super(message, nextHandler);
@@ -59,6 +78,11 @@ export class DbUniqueErrorHandler extends DbErrorHandler {
   }
 }
 
+/**
+ * Chuỗi chain of responsibility các ErrorHandler được tạo ra
+ * dùng builder pattern. Ở chỗ này bản thân cái lớp vừa là
+ * product vừa là builder luôn cơ mà muốn tách ra cũng được.
+ */
 export class DbErrHandlerChain {
   handlers: DbErrorHandler[] = [];
 
